56870b7458cc8aa35d15d3af67572f1df4570a16
Arash-01 <arashnorouzpour01@gmail.com>
2026-04-27 19:19:34 +0330

    Add Dark Souls offline screen and fix paths

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import YouDied from "../components/YouDied";
-
+import YouDied from "./components/YouDied";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
